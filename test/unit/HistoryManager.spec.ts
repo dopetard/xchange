4b3098771068345bf18ce5b75a5d517f08ec0f04
michael1011 <me@michael1011.at>
2018-08-06 10:04:05 +0200

    minor fixes

--- test/unit/HistoryManager.spec.ts
+++ test/unit/HistoryManager.spec.ts
@@ -6,16 +6,20 @@ const testData = require('./values/HistoryManagerData.json');
 function compareArrays(a: number[], b: number[]): boolean {
   if (a.length !== b.length) return false;
 
-  for (const index of a) {
-    if (a[index] !== b[index]) return false;
+  for (const index in a) {
+    if (a[index] !== b[index]) {
+      console.log(`${a[index]} does not equal ${b[index]}`);
+      return false;
+    }
   }
 
   return true;
 }
 
 describe('HistoryManager.parseData', () => {
-  it('should parse data correclty', () => {
+  it('should parse data correctly', () => {
     const output = HistoryManager.parseData(testData.data);
+
     expect(compareArrays(output, testData.expectedOutput)).to.be.true;
   });
 });
