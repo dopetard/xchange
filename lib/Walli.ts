ca97085f2a367fb20af7f172fe14fafb2cf1a5b4
michael1011 <me@michael1011.at>
2018-08-22 11:12:14 +0200

    removed code from custodial approach

--- lib/Walli.ts
+++ lib/Walli.ts
@@ -1,44 +1,19 @@
 import Logger from './Logger';
-import DB, { DBConfig } from './db/DB';
-import XudClient, { XudClientConfig } from './xudclient/XudClient';
-import UserManager from './users/UserManager';
-import API, { APIConfig } from './api/API';
-import HistoryManager from './history/HistoryManager';
 
 type Config = {
   logfile: string,
-  db: DBConfig,
-  xud: XudClientConfig,
-  api: APIConfig,
 };
 
 class Walli {
 
   private logger: Logger;
 
-  constructor(private config: Config) {
+  constructor(config: Config) {
     this.logger = new Logger(config.logfile);
   }
 
   public start = async () => {
-    const db = new DB(this.config.db, this.logger);
-    await db.init();
-
-    const xudClient = new XudClient(this.config.xud, this.logger);
-
-    const historyManager = new HistoryManager(db.models, this.logger);
-    await historyManager.init();
-
-    const userManager = new UserManager({
-      db,
-      xudClient,
-      historyManager,
-      logger: this.logger,
-    });
-    await userManager.init();
-
-    const api = new API(this.config.api, userManager, this.logger);
-    api.init();
+    this.logger.info('test');
   }
 }
 
