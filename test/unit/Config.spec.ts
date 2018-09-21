26182219129e2e414ed200936f68c506fb50b290
Immanuel Segol <3ditds@gmail.com>
2018-09-21 21:53:02 +0300

    over ride test

--- test/unit/Config.spec.ts
+++ test/unit/Config.spec.ts
@@ -1,16 +1,21 @@
 import { expect, assert } from 'chai';
 import { argv } from 'yargs';
 import Config from '../../lib/Config';
+import { deepMerge } from '../../lib/Utils';
+delete argv._;
+delete argv.version;
+delete argv.help;
+delete argv.$0;
+
+const config = new Config();
 
 describe('Config.load() returns proper params', () => {
-  it('has required keys', () => {
-    delete argv._;
-    delete argv.version;
-    delete argv.help;
-    delete argv.$0;
+  it('Should return object', () => {
+    expect(config.load(argv)).to.be.an('object');
+  });
 
-    const config = new Config().load(argv);
-    expect(config).to.be.an('object').that.contains.all.keys(
+  it('Has required keys', () => {
+    expect(config.load(argv)).contains.all.keys(
       'configPath',
       'logPath',
       'logLevel',
@@ -18,5 +23,16 @@ describe('Config.load() returns proper params', () => {
       'btcd',
       'lnd',
       );
+  });
+});
+
+describe('Input over ride order', () => {
+  const cliInput = argv;
+  deepMerge(cliInput, {
+    grpc: { host: '128.0.0.1', port: 2000 },
+  });
+
+  it('Cli input overrides all input', () => {
+    expect(config.load(cliInput)).to.deep.include({ grpc: { host: '128.0.0.1', port: 2000 } });
   });
 });
