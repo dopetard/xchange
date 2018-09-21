5c571bdd401f77f1f1a6c483523b57bd804b8fcd
Immanuel Segol <3ditds@gmail.com>
2018-09-21 21:32:53 +0300

    added basic test

--- test/unit/Config.spec.ts
+++ test/unit/Config.spec.ts
@@ -1,9 +1,22 @@
 import { expect, assert } from 'chai';
+import { argv } from 'yargs';
 import Config from '../../lib/Config';
-import { isObject } from 'util';
 
-describe('new Config()', () => {
+describe('Config.load() returns proper params', () => {
   it('has required keys', () => {
-    expect(new Config()).to.contain.keys('walliDir', 'btcdDir', 'lndDir', 'config');
+    delete argv._;
+    delete argv.version;
+    delete argv.help;
+    delete argv.$0;
+
+    const config = new Config().load(argv);
+    expect(config).to.be.an('object').that.contains.all.keys(
+      'configPath',
+      'logPath',
+      'logLevel',
+      'grpc',
+      'btcd',
+      'lnd',
+      );
   });
 });
