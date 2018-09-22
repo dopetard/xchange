31e1be392f14a4cc1fe5e421df2255bd35231c25
Immanuel Segol <3ditds@gmail.com>
2018-09-22 13:36:26 +0300

    changed walli.proto

--- lib/service/Service.ts
+++ lib/service/Service.ts
@@ -1,18 +1,22 @@
 import Logger from '../Logger';
 import BtcdClient from '../chain/BtcdClient';
+import LndClient from '../lightning/LndClient';
 
 class Service{
   private logger: Logger;
   private btcdClient: BtcdClient;
+  private lndClient: LndClient;
 
-  constructor(logger: Logger, btcdClient: BtcdClient) {
+  constructor(logger: Logger, btcdClient: BtcdClient, lndClient: LndClient) {
     this.logger = logger;
     this.btcdClient = btcdClient;
+    this.lndClient = lndClient;
   }
 
   public getInfo = async (): Promise<object> => {
-    const info = await this.btcdClient.getInfo();
-    return info;
+    const btcdInfo = await this.btcdClient.getInfo();
+    const lndInfo = await this.lndClient.getInfo();
+    return btcdInfo;
   }
 }
 
