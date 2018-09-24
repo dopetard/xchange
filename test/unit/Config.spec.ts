dc4e9dacae53f04370630259b350922b73b8c91a
Immanuel Segol <3ditds@gmail.com>
2018-09-24 15:21:07 +0300

    refactor(Config.spec.ts)changed test

--- test/unit/Config.spec.ts
+++ test/unit/Config.spec.ts
@@ -3,37 +3,66 @@ import { argv } from 'yargs';
 import Config from '../../lib/Config';
 import { deepMerge } from '../../lib/Utils';
 
-delete argv._;
-delete argv.version;
-delete argv.help;
-delete argv.$0;
-
 const config = new Config();
 
-describe('Config.load() returns proper params', () => {
-  it('Should return object', () => {
+describe('Config', () => {
+  const cliInput = argv;
+
+  delete cliInput._;
+  delete cliInput.version;
+  delete cliInput.help;
+  delete cliInput.$0;
+  delete cliInput.r;
+
+  const ops = {
+    configPath: '',
+    walletPath: '',
+    logPath: '',
+    walliDir: '',
+    logLevel: '',
+    grpc: {
+      host: '127.0.0.1',
+      port: 9000,
+    },
+    btcd: {
+      host: '127.0.0.1',
+      port: 18334,
+      rpcuser: '',
+      rpcpass: '',
+      configPath: '',
+      certPath: '',
+      rpclimituser: 'limituser',
+      rpclimitpass: 'limituser',
+      testnet: '1',
+      datadir: 'F:\\testnet',
+    },
+    lnd: {
+      host: '127.0.0.1',
+      port: 10009,
+      certPath: '',
+      macaroonPath: '',
+      configPath: '',
+    },
+  };
+  deepMerge(cliInput, ops);
+
+  it('should return object', () => {
     expect(config.load(argv)).to.be.an('object');
   });
 
-  it('Has required keys', () => {
+  it('should have required keys', () => {
     expect(config.load(argv)).contains.all.keys(
       'configPath',
       'logPath',
+      'walliDir',
       'logLevel',
       'grpc',
       'btcd',
       'lnd',
       );
   });
-});
 
-describe('Input over ride order', () => {
-  const cliInput = argv;
-  deepMerge(cliInput, {
-    grpc: { host: '128.0.0.1', port: 2000 },
-  });
-
-  it('Cli input overrides all options', () => {
-    expect(config.load(cliInput)).to.deep.include({ grpc: { host: '128.0.0.1', port: 2000 } });
+  it('should override all options from cli', () => {
+    expect(config.load(cliInput)).to.deep.include(ops);
   });
 });
