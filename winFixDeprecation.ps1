cfdf41d67d1367417d739044fc86735b74f13331
Immanuel Segol <3ditds@gmail.com>
2018-10-02 23:44:01 +0300

    chore(powershell) unnecessary commands

--- winFixDeprecation.ps1
+++ winFixDeprecation.ps1
@@ -3,5 +3,5 @@ $OldString  = 'new Buffer'
 $NewString  = 'Buffer.from'
 $InputFiles | ForEach {
     (Get-Content -Path $_.FullName).Replace($OldString,$NewString) | Set-Content -Path $_.FullName
-     Write-Output "`n" | Out-File $_.FullName -encoding ASCII -append
+
 }
