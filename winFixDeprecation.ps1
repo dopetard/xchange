f1ae9b9baf2dc9d6f44192bd8b22235f8a3aecde
Immanuel Segol <3ditds@gmail.com>
2018-10-04 22:34:54 +0300

    chore(winFix) cleanup

--- winFixDeprecation.ps1
+++ winFixDeprecation.ps1
@@ -3,5 +3,4 @@ $OldString  = 'new Buffer'
 $NewString  = 'Buffer.from'
 $InputFiles | ForEach {
     (Get-Content -Path $_.FullName).Replace($OldString,$NewString) | Set-Content -Path $_.FullName
-
 }
